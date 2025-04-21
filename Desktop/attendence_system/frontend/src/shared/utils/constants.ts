14542052478f51257a03ca9f8c9060d9bbf9d7ec
Om Sherikar <omsherikar0229@gmail.com>
2025-04-21 21:24:40 +0530

    Delete Desktop/attendence_system/frontend directory

--- Desktop/attendence_system/frontend/src/shared/utils/constants.ts
+++
@@ -1,8 +0,0 @@
-export const WEBSOCKET_EVENTS = {
-  JOIN_CLASS: 'joinClass',
-  LEAVE_CLASS: 'leaveClass',
-  MARK_ATTENDANCE: 'markAttendance',
-  SYNC_REQUEST: 'syncRequest',
-  ATTENDANCE_UPDATE: 'attendanceUpdate',
-  ERROR: 'error',
-}; 
